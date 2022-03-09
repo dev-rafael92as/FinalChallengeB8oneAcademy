b6eaeb5133060ece725a7ee4384c5c25ff40b5ab
Rafael Barros de Almeida e Silva <rafa_barros92@hotmail.com>
2022-03-08 22:17:29 -0300

    Inicio Implementação JS

---
+++ scripts/authPage.js
@@ -0,0 +1,16 @@
+// Lógica para validação do login
+
+const loginForm = document.querySelector(".auth-page__submit");
+
+let getEmailInput = document.getElementById('email');
+
+const getPasswordInput = document.getElementById("password");
+
+
+loginForm.addEventListener('click', event => {
+    event.preventDefault();
+
+    
+})
+
+
